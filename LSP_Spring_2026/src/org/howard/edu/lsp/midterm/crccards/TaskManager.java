8fd5de0af934b40b2b1c3b221de5e8bff64d3c36
AKeys05 <amaya.keys@bison.howard.edu>
2026-03-19 18:54:40 -0400

    Configure constructor

--- LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/TaskManager.java
+++ LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/TaskManager.java
@@ -1,10 +1,26 @@
 package org.howard.edu.lsp.midterm.crccards;
 
+/**
+ * Name: Amaya Keys
+ * Manages a collection of {@link Task} objects.
+ * Supports adding tasks, locating a task by ID, and retrieving tasks by status.
+ * Duplicate task IDs are not permitted.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TaskManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	private Map<String, Task> tasks;
+	
+	/**
+     * Constructs a new TaskManager with an empty task collection.
+     */
+    public TaskManager() {
+        tasks = new HashMap<>();
+    }
 
 }
