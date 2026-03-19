1a2c58e50df0f64f12196f6a1c9d23556a534052
AKeys05 <amaya.keys@bison.howard.edu>
2026-03-19 18:51:23 -0400

    Configure constructor

--- LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/Task.java
+++ LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/Task.java
@@ -1,10 +1,29 @@
 package org.howard.edu.lsp.midterm.crccards;
 
+/**
+ * Name: Amaya Keys
+ * Represents a task with an ID, description, and status.
+ * The default status upon creation is "OPEN".
+ * Valid status values are: OPEN, IN_PROGRESS, COMPLETE.
+ */
+
 public class Task {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private String taskId;
+    private String description;
+    private String status;
+    
+    /**
+     * Constructs a new Task with the given ID and description.
+     * The status is initialized to "OPEN" by default.
+     *
+     * @param taskId      the unique identifier for this task
+     * @param description a brief description of the task
+     */
+	public Task(String taskId, String description) {
+		this.taskId = taskId;
+        this.description = description;
+        this.status = "OPEN";
 	}
 
 }
