07578a6e97a24bda4c325045465f5e090c113a71
AKeys05 <amaya.keys@bison.howard.edu>
2026-03-19 18:48:57 -0400

    Initial setup of CRC classes

---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/Task.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.crccards;
+
+public class Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/midterm/crccards/TaskManager.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.crccards;
+
+public class TaskManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
